974f070156146c2b7451367ad3fb7d7c7686e38f
Vishnu <vishnunarang2000@gmail.com>
2018-02-14 11:21:29 -0700

    Basic setup based on DialogFlow sdk

---
+++ js/text-to-speech.js
@@ -0,0 +1,61 @@
+var speechSynthesis = window.mozSpeechSynthesis || window.msSpeechSynthesis || window.oSpeechSynthesis || window.webkitSpeechSynthesis || window.speechSynthesis,
+  ss, isResult = false;
+
+var voices = [];
+
+if (speechSynthesis !== undefined) {
+
+  function populateVoiceList() {
+    voices = speechSynthesis.getVoices();
+  }
+
+  populateVoiceList();
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  function start(text, params) {
+
+    var utterThis = new SpeechSynthesisUtterance(text);
+    utterThis.pitch = params.pitch || 1;
+    utterThis.rate = params.rate || 1;
+    utterThis.voice = params.voice || 'English';
+    speechSynthesis.speak(utterThis);
+
+    utterThis.onend = function(event) {
+      console.log('Finished in ' + event.elapsedTime + ' seconds.');
+    }
+    utterThis.onpause = function(event) {
+      var char = event.utterance.text.charAt(event.charIndex);
+      console.log('Speech paused at character ' + event.charIndex + ' of "' +
+        event.utterance.text + '", which is "' + char + '".');
+    };
+
+  };
+
+  function speakResponse(response) {
+    event.preventDefault();
+
+    // var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    var selectedOption = 'Samantha';
+    var params = {
+      voice: null,
+      rate: 1,
+      pitch: 1
+    };
+
+    for (i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        params.voice = voices[i];
+      }
+    }
+
+    start(response, params);
+
+    return false;
+  }
+
+} else {
+  alert('Your browser don\'t support Web Speech Synthesis API');
+}
