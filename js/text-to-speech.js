6207670245163d817496b512d587640cbde4ad67
Vishnu Narang <vishnunarang2000@gmail.com>
2018-02-26 15:22:01 -0700

    Adding a backup voice

--- js/text-to-speech.js
+++ js/text-to-speech.js
@@ -1,61 +1,65 @@
 var speechSynthesis = window.mozSpeechSynthesis || window.msSpeechSynthesis || window.oSpeechSynthesis || window.webkitSpeechSynthesis || window.speechSynthesis,
-  ss, isResult = false;
+	ss, isResult = false;
 
 var voices = [];
 
 if (speechSynthesis !== undefined) {
 
-  function populateVoiceList() {
-    voices = speechSynthesis.getVoices();
-  }
+	function populateVoiceList() {
+		voices = speechSynthesis.getVoices();
+	}
 
-  populateVoiceList();
+	populateVoiceList();
 
-  if (speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = populateVoiceList;
-  }
+	if (speechSynthesis.onvoiceschanged !== undefined) {
+		speechSynthesis.onvoiceschanged = populateVoiceList;
+	}
 
-  function start(text, params) {
+	function start(text, params) {
 
-    var utterThis = new SpeechSynthesisUtterance(text);
-    utterThis.pitch = params.pitch || 1;
-    utterThis.rate = params.rate || 1;
-    utterThis.voice = params.voice || 'English';
-    speechSynthesis.speak(utterThis);
+		var utterThis = new SpeechSynthesisUtterance(text);
+		utterThis.pitch = params.pitch || 1;
+		utterThis.rate = params.rate || 1;
+		utterThis.voice = params.voice || 'English';
+		speechSynthesis.speak(utterThis);
 
-    utterThis.onend = function(event) {
-      console.log('Finished in ' + event.elapsedTime + ' seconds.');
-    }
-    utterThis.onpause = function(event) {
-      var char = event.utterance.text.charAt(event.charIndex);
-      console.log('Speech paused at character ' + event.charIndex + ' of "' +
-        event.utterance.text + '", which is "' + char + '".');
-    };
+		utterThis.onend = function(event) {
+			console.log('Finished in ' + event.elapsedTime + ' seconds.');
+		}
+		utterThis.onpause = function(event) {
+			var char = event.utterance.text.charAt(event.charIndex);
+			console.log('Speech paused at character ' + event.charIndex + ' of "' +
+				event.utterance.text + '", which is "' + char + '".');
+		};
 
-  };
+	};
 
-  function speakResponse(response) {
-    event.preventDefault();
+	function speakResponse(response) {
+		event.preventDefault();
 
-    // var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
-    var selectedOption = 'Samantha';
-    var params = {
-      voice: null,
-      rate: 1,
-      pitch: 1
-    };
+		// var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+		var selectedOption = 'Samantha';
+		var params = {
+			voice: null,
+			rate: 1,
+			pitch: 1
+		};
 
-    for (i = 0; i < voices.length; i++) {
-      if (voices[i].name === selectedOption) {
-        params.voice = voices[i];
-      }
-    }
+		for (i = 0; i < voices.length; i++) {
+			if (voices[i].name === selectedOption) {
+				params.voice = voices[i];
+			}
+		}
 
-    start(response, params);
+		if (not params.voice) {
+			params.voice = voices[1];
+		}
 
-    return false;
-  }
+		start(response, params);
+
+		return false;
+	}
 
 } else {
-  alert('Your browser don\'t support Web Speech Synthesis API');
+	alert('Your browser don\'t support Web Speech Synthesis API');
 }
